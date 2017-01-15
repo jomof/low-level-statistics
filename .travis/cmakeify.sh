cbee60f12fe330f06c8f6fc691e3d982ef9f06c7
Jomo Fisher (windows) <jomofisher@gmail.com>
2017-01-15 14:51:29 -0800

    .

--- .travis/cmakeify.sh
+++ .travis/cmakeify.sh
@@ -10,7 +10,8 @@ echo --"$CMAKE_SYSTEM_NAME"--
 case "$CMAKE_SYSTEM_NAME" in
 Windows)
   echo --DOWNLOADING--
-  wget https://github.com/jomof/cmakeify/releases/download/mingw-w64/mingw-w64-bin_x86_64-linux_20131228.tar.bz2 -O mingw-w64.tar.bz2/download
+  wget https://github.com/jomof/cmakeify/releases/download/mingw-w64/mingw-w64-bin_x86_64-linux_20131228.tar.bz2 -O mingw-w64.tar.bz2
+  tar xvfz mingw-w64.tar.bz2 -C prebuilts/
   ;;
 esac
 
