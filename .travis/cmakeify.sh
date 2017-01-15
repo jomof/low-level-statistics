13f0423654a9478447962113da922c04bac51dee
Jomo Fisher (windows) <jomofisher@gmail.com>
2017-01-15 11:48:58 -0800

    .

--- .travis/cmakeify.sh
+++ .travis/cmakeify.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 # cmakeify installs and runs CMake with different configurations
 
+#if [[ "$(uname -s)" == 'Darwin' ]]; then
+
+
+# install a CMake 
+wget --no-check-certificate https://cmake.org/files/dev/cmake-3.7.20161217-g65aad-Linux-x86_64.tar.gz
+tar xvfz cmake-3.7.20161217-g65aad-Linux-x86_64.tar.gz -C prebuilts/
+
