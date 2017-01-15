36cafc4cf351c9580be7238d51ec32795a587423
Jomo Fisher (windows) <jomofisher@gmail.com>
2017-01-15 13:18:03 -0800

    .

--- .travis/cmakeify.sh
+++ .travis/cmakeify.sh
@@ -5,7 +5,7 @@
 CMAKEIFY_CMAKE_VERSION=cmake-3.7.20161217-g65aad
 
 # install cross-targeting prereqs
-if ["${CMAKE_SYSTEM_NAME}" == "Windows"]; then
+if ["${CMAKE_SYSTEM_NAME}" -eq 'Windows']; then
   wget http://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win64/Automated%20Builds/mingw-w64-bin_x86_64-linux_20131228.tar.bz2/download -O mingw-w64.tar.bz2/download
 fi
 
