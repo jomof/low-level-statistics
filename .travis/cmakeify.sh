d2d55f30466cfc93591cbdf4ea1a954306247739
Jomo Fisher (windows) <jomofisher@gmail.com>
2017-01-15 14:55:58 -0800

    .

--- .travis/cmakeify.sh
+++ .travis/cmakeify.sh
@@ -4,21 +4,6 @@
 
 CMAKEIFY_CMAKE_VERSION=cmake-3.7.20161217-g65aad
 
-# install cross-targeting prereqs
-echo --"$CMAKE_SYSTEM_NAME"--
-
-case "$CMAKE_SYSTEM_NAME" in
-Windows)
-  echo --DOWNLOADING--
-  wget https://github.com/jomof/cmakeify/releases/download/mingw-w64/mingw-w64-bin_x86_64-linux_20131228.tar.bz2 -O mingw-w64.tar.bz2
-  tar xvfj mingw-w64.tar.bz2 -C prebuilts/
-  ;;
-esac
-
-
-
-
-
 # install a CMake 
 mkdir prebuilts/
 if [[ "$(uname -s)" == 'Darwin' ]]; then
@@ -30,10 +15,16 @@ wget --no-check-certificate https://cmake.org/files/dev/${CMAKEIFY_CMAKE_VERSION
 tar xvfz cmake.tar.gz -C prebuilts/ > untar.cmake.txt
 mv prebuilts/${CMAKEIFY_CMAKE_VERSION}-Linux-x86_64 prebuilts/cmake
 fi
-
 prebuilts/cmake/bin/cmake --version
 
-
+# install cross-targeting prereqs
+case "$CMAKE_SYSTEM_NAME" in
+Windows)
+  echo --DOWNLOADING--
+  wget https://github.com/jomof/cmakeify/releases/download/mingw-w64/mingw-w64-bin_x86_64-linux_20131228.tar.bz2 -O mingw-w64.tar.bz2
+  tar xvfj mingw-w64.tar.bz2 -C prebuilts/
+  ;;
+esac
 
 # build
 mkdir build/
