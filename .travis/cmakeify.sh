65409213cf1368766bc058248a95977ae01c01f9
Jomo Fisher (windows) <jomofisher@gmail.com>
2017-01-15 12:31:37 -0800

    .

--- .travis/cmakeify.sh
+++ .travis/cmakeify.sh
@@ -16,7 +16,7 @@ mv ${CMAKEIFY_CMAKE_VERSION}-Darwin-x86_64 cmake
 else
 wget --no-check-certificate https://cmake.org/files/dev/${CMAKEIFY_CMAKE_VERSION}-Linux-x86_64.tar.gz -O cmake.tar.gz
 tar xvfz cmake.tar.gz -C prebuilts/
-mv ${CMAKEIFY_CMAKE_VERSION}-Linux-x86_64 cmake
+mv prebuilts/${CMAKEIFY_CMAKE_VERSION}-Linux-x86_64 prebuilts/cmake
 fi
 
 prebuilts/cmake/bin/cmake --version
