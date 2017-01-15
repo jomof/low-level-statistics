91eeeab445e58fae34fb6d62b3c21702129298dd
Jomo Fisher (windows) <jomofisher@gmail.com>
2017-01-15 12:03:34 -0800

    .

--- .travis/cmakeify.sh
+++ .travis/cmakeify.sh
@@ -3,9 +3,16 @@
 
 #if [[ "$(uname -s)" == 'Darwin' ]]; then
 
+CMAKEIFY_CMAKE_VERSION=cmake-3.7.20161217-g65aad-Linux-x86_64
 
 # install a CMake 
 mkdir prebuilts/
-wget --no-check-certificate https://cmake.org/files/dev/cmake-3.7.20161217-g65aad-Linux-x86_64.tar.gz
-tar xvfz cmake-3.7.20161217-g65aad-Linux-x86_64.tar.gz -C prebuilts/
+wget --no-check-certificate https://cmake.org/files/dev/${CMAKEIFY_CMAKE_VERSION}.tar.gz
+tar xvfz ${CMAKEIFY_CMAKE_VERSION}.tar.gz -C prebuilts/
 
+
+# build
+mkdir build/
+cd build
+../prebuilts/${CMAKEIFY_CMAKE_VERSION}/bin/cmake ..
+make
