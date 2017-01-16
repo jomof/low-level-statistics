b0a0f18a54c1988eac4d0a4c59c76d2ed542c50a
Jomo Fisher (windows) <jomofisher@gmail.com>
2017-01-15 18:06:41 -0800

    .

--- .travis/cmakeify.sh
+++ .travis/cmakeify.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 # cmakeify installs and runs CMake with different configurations
 
-
 CMAKEIFY_CMAKE_VERSION=cmake-3.7.20161217-g65aad
 
 CMAKE_C_COMPILER=/usr/bin/cc
 CMAKE_CXX_COMPILER=/usr/bin/c++
-
-
 
 # install a CMake 
 mkdir prebuilts/
@@ -22,14 +19,25 @@ mv prebuilts/${CMAKEIFY_CMAKE_VERSION}-Linux-x86_64 prebuilts/cmake
 fi
 prebuilts/cmake/bin/cmake --version
 
-# install cross-targeting prereqs
+# install compiler prereqs
 case "$CMAKE_SYSTEM_NAME" in
 Windows)
-  mkdir prebuilts/mingw-w64
-  wget https://github.com/jomof/cmakeify/releases/download/mingw-w64/mingw-w64-bin_x86_64-linux_20131228.tar.bz2 -O mingw-w64.tar.bz2
-  tar xvfj mingw-w64.tar.bz2 -C prebuilts/mingw-w64 > untar.mingw-w64.txt
-  CMAKE_CXX_COMPILER=$PWD/prebuilts/mingw-w64/bin/x86_64-w64-mingw32-g++
-  CMAKE_C_COMPILER=$PWD/prebuilts/mingw-w64/bin/x86_64-w64-mingw32-gcc
+  case "$CMAKEIFY_GCC" in
+  4.9.0)
+    mkdir prebuilts/mingw-w64
+    wget https://github.com/jomof/cmakeify/releases/download/mingw-w64/mingw-w64-bin_x86_64-linux_20131228.tar.bz2 -O mingw-w64.tar.bz2
+    tar xvfj mingw-w64.tar.bz2 -C prebuilts/mingw-w64 > untar.mingw-w64.txt
+    CMAKE_CXX_COMPILER=$PWD/prebuilts/mingw-w64/bin/x86_64-w64-mingw32-g++
+    CMAKE_C_COMPILER=$PWD/prebuilts/mingw-w64/bin/x86_64-w64-mingw32-gcc
+    ;;
+  *)
+    echo $"GCC version CMAKEIFY_GCC=$CMAKEIFY_GCC is not supported. Use 4.9.0"
+    exit 1
+  esac
+  ;;
+Linux)
+  wget http://mirrors-usa.go-parts.com/gcc/releases/gcc-${CMAKEIFY_GCC}/gcc-${CMAKEIFY_GCC}.tar.bz2 -O gcc.tar.bz2
+  tar xvfj gcc.tar.bz2
   ;;
 esac
 
