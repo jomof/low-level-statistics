5df7970358dd102f34902affeac7f1bc6ff16aac
Jomo Fisher <jomofisher@gmail.com>
2017-01-28 11:11:17 -0800

    Update statistics.c

--- statistics.c
+++ statistics.c
@@ -1,6 +1,7 @@
 #include "statistics.h"
 
-double jaf_sum(double * numbers, int size) {
+double jaf_sum(double * numbers, 
+	       int size) {
 	double sum = 0;
 	for (int i = 0; i < size; ++i) {
 		sum += numbers[i];
